b63647c80bd26aac5708db313acb7019decbffaa
Omar Khan <omaralikhan4518@gmail.com>
2026-06-05 01:13:55 +0530

    Changes made

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,23 +1,17 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  socket: {
-    host: "127.0.0.1",
-    port: 6379,
-  },
-
-  RESP: 2
+  url: process.env.REDIS_URL,
 });
 
 redisClient.on("error", (err) => {
   console.error("Redis Error:", err);
 });
 
-try {
-  await redisClient.connect();
+redisClient.on("connect", () => {
   console.log("✅ Redis Connected");
-} catch (err) {
-  console.error("❌ Redis Connection Failed:", err);
-}
+});
+
+await redisClient.connect();
 
 export default redisClient;
